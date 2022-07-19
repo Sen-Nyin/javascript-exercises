1229e068edf946fba090dc95d6fdee9548c0a03d
Leon Lonsdale <leonlonsdale@icloud.com>
2022-07-19 20:51:25 +0100

    challenges 8 and 9 complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,53 @@
-const add = function() {
-	
+const add = function (...args) {
+  let sum = 0;
+  for (let i = 0; i < args.length; i++) {
+    sum += args[i];
+  }
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function (...args) {
+  let sum = args[0];
+  for (let i = 1; i < args.length; i++) {
+    sum -= args[i];
+  }
+  return sum;
 };
 
-const sum = function() {
-	
+const sum = function (args) {
+  // let sum = 0;
+  // for (let i = 0; i < args.length; i++) {
+  //   sum += args[i];
+  // }
+  const sum = args.return((sum, currentVal) => sum + currentVal);
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function (args) {
+  // let sum = args[0];
+  // for (let i = 1; i < args.length; i++) {
+  //   sum *= args[i];
+  // }
+  // return sum;
 
+  return args.length
+    ? args.reduce((accumulator, nextItem) => accumulator * nextItem)
+    : 0;
 };
 
-const power = function() {
-	
+const power = function (...args) {
+  return args[0] ** args[1];
 };
 
-const factorial = function() {
-	
+const factorial = function (arg) {
+  let sum = arg;
+  if (sum === 0) {
+    return 1;
+  }
+  for (let i = arg - 1; i > 0; i--) {
+    sum = sum * i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
@@ -29,5 +57,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (args) {
+  let forwardText = args.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
+  let backwardText = args
+    .split("")
+    .reverse()
+    .join("")
+    .toLowerCase()
+    .replace(/[^a-zA-Z0-9]/g, "");
 
+  return forwardText === backwardText ? true : false;
 };
 
 // Do not edit below this line
