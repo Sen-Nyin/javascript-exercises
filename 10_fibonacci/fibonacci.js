d1fbdf94e87f97bdc1406236578b902f0ff8ae69
Leon Lonsdale <leonlonsdale@icloud.com>
2022-07-20 13:13:30 +0100

    challenges 10 & 11 done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  if (num < 0) return "OOPS";
 
+  const fib = [];
+  for (let i = 0; i <= num; i++) {
+    if (i === 0 || i === 1) {
+      fib.push(i);
+    } else {
+      fib.push(fib[i - 1] + fib[i - 2]);
+    }
+  }
+  return fib[num];
 };
-
 // Do not edit below this line
 module.exports = fibonacci;
--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function (books) {
+  //   const titles = [];
+  //   for (let i = 0; i < books.length; i++) {
+  //     titles.push(books[i].title);
+  //   }
+  //   return titles;
 
+  return books.map((bookTitles) => bookTitles.title);
 };
 
 // Do not edit below this line
