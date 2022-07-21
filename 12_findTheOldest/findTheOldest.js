1592bb18d6500aef5a6b2dd9b488d8f800c7c76e
Leon Lonsdale <leonlonsdale@icloud.com>
2022-07-21 10:58:38 +0100

    challenge 12 done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,31 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+  //   let age = 0;
+  //   return arr.reduce((oldest, person) => {
+  //     if (person.yearOfDeath) {
+  //       if (person.yearOfDeath - person.yearOfBirth > age) {
+  //         age = person.yearOfDeath - person.yearOfBirth;
+  //         return person;
+  //       }
+  //     } else {
+  //       let year = new Date().getFullYear();
+  //       if (year - person.yearOfBirth > age) {
+  //         age = year - person.yearOfBirth;
+  //         return person;
+  //       }
+  //     }
+  //     return oldest;
+  //   }, 0);
 
+  return arr.reduce((oldest, person) => {
+    const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+    const currentAge = getAge(person.yearOfBirth, person.yearOfDeath);
+    return currentAge > oldestAge ? person : oldest;
+  });
+};
+
+const getAge = (birth, death) => {
+  if (!death) death = new Date().getFullYear();
+  return death - birth;
 };
 
 // Do not edit below this line
