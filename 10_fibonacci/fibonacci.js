a7e2427d914e8f08b1f491e2fec37821473d9330
Leon Lonsdale <leonlonsdale@icloud.com>
2022-08-05 00:22:54 +0100

    fix 'oops'

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -13,7 +13,7 @@
 // };
 
 const fibonacci = function (num) {
-  num < 0 && "OOPS";
+  if (num < 0) return "OOPS";
   const fib = [];
   for (let i = 0; i <= num; i++) {
     i > 1 ? fib.push(fib[i - 1] + fib[i - 2]) : fib.push(i);
