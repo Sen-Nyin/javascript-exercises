600c09a30004b67ffdf135f7834b7042f01127e9
Leon Lonsdale <leonlonsdale@icloud.com>
2022-08-04 16:01:01 +0100

    smaller fibonacci code

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,13 +1,22 @@
-const fibonacci = function (num) {
-  if (num < 0) return "OOPS";
+// const fibonacci = function (num) {
+//   if (num < 0) return "OOPS";
 
+//   const fib = [];
+//   for (let i = 0; i <= num; i++) {
+//     if (i === 0 || i === 1) {
+//       fib.push(i);
+//     } else {
+//       fib.push(fib[i - 1] + fib[i - 2]);
+//     }
+//   }
+//   return fib[num];
+// };
+
+const fibonacci = function (num) {
+  num < 0 && "OOPS";
   const fib = [];
   for (let i = 0; i <= num; i++) {
-    if (i === 0 || i === 1) {
-      fib.push(i);
-    } else {
-      fib.push(fib[i - 1] + fib[i - 2]);
-    }
+    i > 1 ? fib.push(fib[i - 1] + fib[i - 2]) : fib.push(i);
   }
   return fib[num];
 };
